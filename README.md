2f32e220645f1e37c6d083ff2a771dfb694e2d6d
aangl3r <justinmchastain@gmail.com>
2019-05-03 12:20:02 -0400

    basic functionality complete for spotify, omdb, and bands in town

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 https://aangl3r.github.io/liri-node-app/
 
+***Spotify
+Screenshot of liri.js taking multiple words as input for and returning song information
+via the spotify API.
+![](assets/images/spotify-this-song-working.JPG)
+
+If no song is given, spotify returns this song "Good Christians Don't Get Jiggy With It
+'Til After Marriage"
+![](assets/images/spotify-no-argument.JPG)
+
+***OMDB
+Screenshot of liri.js taking multiple words as an input and logging the information
+about the movie to the console.
 ![](assets/images/working-omdb-multi-word-input.JPG)
 
+Screenshot showing Mr Nobody output when user does not enter a movie name
+![](assets/images/mr-nobody-blank-movie.JPG)
+
+***Bands in Town
+Screenshot of liri.js taking multiple words as an input and logging the information
+about the artists upcoming shows to the console.
+![](assets/images/concert-this-working.JPG)
+
+If no argument is entered, a simple error message is returned
+![](assets/images/concert-this-error.JPG)
--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotifyAPI = require("node-spotify-api");
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var logFile = "./log.txt";
 var request = require("request");
+var moment = require("moment");
 
 
 //npm simple-node-logger
@@ -46,9 +46,13 @@ switch (command) {
 
 //movie retrieval from OMDB
 function doMovie() {
+    if (!argument) {
+        argument = "Mr. Nobody"
+    }
     var queryUrl = "http://www.omdbapi.com/?t=" + argument + "&y=&plot=short&apikey=trilogy";
 
     request(queryUrl, function (error, response, body) {
+
         if (!error && response.statusCode === 200) {
             var body = JSON.parse(body);
 
@@ -67,3 +71,77 @@ function doMovie() {
     })
 }
 
+function doSpotify() {
+    var spotify = new Spotify(keys.spotify);
+    if (!argument) {
+        argument = "Good Christians Don't Get Jiggy With It 'Til After Marriage"
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: argument
+        },
+        function (error, data) {
+            if (error) {
+                console.log("error")
+                return;
+            }
+            var song = data.tracks.items;
+
+            console.log("Song name: " + song[0].name);
+            console.log("Artist: " + song[0].artists[0].name);
+            console.log("Preview: " + song[0].preview_url);
+            console.log("Album: " + song[0].album.name);
+            console.log("__________End_Of_Data__________");
+
+        }
+    )
+}
+
+function doConcert() {
+    if (!argument) {
+        console.log("enter an argument")
+    }
+    var queryUrl = "https://rest.bandsintown.com/artists/" + argument + "/events?app_id=codingbootcamp";
+
+    request(queryUrl, function (error, response, data) {
+
+        if (!error && response.statusCode === 200) {
+            var data = JSON.parse(data);
+
+            for (var i = 0; i < data.length; i++){
+
+            console.log(data[i].venue.name);
+            console.log(data[i].venue.city + ", " + data[i].venue.country);
+            console.log(moment(data[i].datetime).format("L"));
+            console.log("____________________")
+            }
+        }
+        else {
+            console.log("error");
+        }
+    })
+}
+
+function doWhat() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log(error);
+        }
+
+        var arr = data.split(", ");
+
+        if (arr[0] === "spotify-this-song") {
+            var getSong = arr[1].slice(1, -1);
+            doSpotify(getSong);
+        }
+        else if (arr[0] === "movie-this") {
+            var getMovie = arr[1].slice(1, -1);
+            doMovie(getMovie);
+        }
+        else if (arr[0] === "concert-this") {
+            var getConcert = arr[1].slice(1, -1);
+            doConcert(getConcert);
+        }
+    });
+}
